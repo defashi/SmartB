9c1e8b404d13ebddcbd63d099a9a875d3760516a
defashi <davidfaustin74.df@gmail.com>
2018-05-06 16:42:30 +0300

    div size from 12 to 8

--- resources/views/pages/reguser.blade.php
+++ resources/views/pages/reguser.blade.php
@@ -2,6 +2,7 @@
 
  @section('content')
   <h3>User Registration Page</h3>
+  <div class="col-md-8">
   {!! Form::open(['url' => 'foo/bar']) !!}
     <div class="form-group">
     	{{form::label('name', 'Name')}}
@@ -16,9 +17,10 @@
     	{{form::text('amount', '', ['class' => 'form-control', 'placeholder' => 'amount'])}}
     </div>
     <div class="form-group">
-    	{{form::label('category', 'Category')}}
+    	{{form::label('category', 'Category')}} &nbsp;
     	{{Form::select('category', ['s' => 'Student', 'n' => 'Normal'], 'S')}}
     </div>
     {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
   {!! Form::close() !!}
+  </div>
  @endsection
