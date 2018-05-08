b44b977befd6264b61b15e4f2e63f71b761b706b
defashi <davidfaustin74.df@gmail.com>
2018-05-08 17:40:11 +0300

    modify homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.appp')
 
  @section('content')
-  <h3>home page</h3>
-  <p>welcome to SmartCityBus...</p>
+  
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4><b>Welcome To SmartBusCity</b></h4></div>
+
+                <div class="panel-body">
+                    <p>SmartBusCity is the system that allows a bus boarder with RFID card to swipe the RFID card in proximity with RFID card reader for trip payment in a particular city bus as well as being able to recharge his or her account with a mobile money service.</p><hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
  @endsection
